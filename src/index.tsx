ba2348245988b47b9dec15e24170a2874dbee5f3
dlford <dan-github2@dlford.co>
2021-02-11 15:35:07 -0700

    config: set up Goober

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import { h, render } from 'preact'
+import { setup } from 'goober'
 import 'preact/devtools'
+
 import App from './App.js'
 import './index.css'
+
+setup(h)
 
 const root = document.getElementById('root')
 
