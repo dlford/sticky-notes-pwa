256ff6127f4ca7ea6a6fb18cee14b156d44ace34
dlford <dan-github2@dlford.co>
2021-02-13 10:11:50 -0700

    start on CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { h, render } from 'preact'
 import { setup } from 'goober'
 import 'preact/devtools'
+import '~/styles/normalize.css'
+import '~/styles/index.css'
 
 import App from './App.js'
 
